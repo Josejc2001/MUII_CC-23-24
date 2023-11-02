d54c60c6c8fc4dd9a46adbb0c9e15fd08ae37560
Jose Jimenez Cazorla <josejc2001@gmail.com>
2023-11-02 11:32:10 +0100

    Continuación estructura inicial Jugador #11

--- backend/src/main/java/com/royalfantasy/backend/domain/model/Player.java
+++ backend/src/main/java/com/royalfantasy/backend/domain/model/Player.java
@@ -16,4 +16,78 @@ public class Player {
     private Integer points;
     // Team
     // Img
+    public Player() {
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getClause() {
+        return clause;
+    }
+
+    public void setClause(Integer clause) {
+        this.clause = clause;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
 }
